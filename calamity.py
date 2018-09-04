d072450f6df44e3e87ed4c186c96a9901bf24aea
Mahmoud Khaled <mahmoud_khld@yahoo.com>
2018-09-04 13:42:36 +0200

    minor changes in Calamity
    removes duplicate URLs before fetching
    load appends to the payload
    avoid re-adding existing articles

--- calamity.py
+++ calamity.py
@@ -15,7 +15,7 @@ class Calamity:
         self.filepath = filepath
         self.urls_skipped = []
         try:
-            Calamity._load(self, filepath)
+            Calamity.load(self, filepath)
             print('Object has been successfully loaded')
         except:
             print(filepath, 'not found, nvm creating a new empty one')
@@ -34,8 +34,25 @@ class Calamity:
     #         'raw' : a.html
         }
     
-    def _load(self, f):
-        self.payload = eval(open(f, 'r').read())
+    def load(self, f):
+        new_payload = eval(open(f, 'r').read())
+        existing_metas = [x['meta'] for x in self.payload]
+        
+        for event in new_payload:
+            if event['meta'] in existing_metas:
+                idx = existing_metas.index(event['meta'])
+                existing_articles = self.payload[idx]['articles']
+                existing_articles_url = [x['url'] for x in self.payload[idx]['articles']]
+                article_count = 0
+                for article in event['articles']:
+                    if article['url'] not in existing_articles_url:
+                        existing_articles.append(article)
+                        article_count += 1
+                if article_count:
+                    print('[+] Added', article_count, 'articles to exisiting event', event['meta']['label'])
+            else:
+                print('[+] New event', event['meta']['label'], 'had been added')
+                self.payload.append(event)
             
     def urlRead(f):
         return list(filter(None, open(f, 'r').read().split('\n')))
@@ -46,7 +63,7 @@ class Calamity:
         try:
             open(f + '-' +  str(int(time.time())) + '.lobj', 'w').write(str(self.payload))
         except:
-            print('Failed to write the Object')
+            print('[X] Failed to write the Object')
             
     def overwrite(self):
         f = self.filepath
@@ -54,7 +71,7 @@ class Calamity:
         try:
             open(f, 'w').write(str(self.payload))
         except:
-            print('Failed to write the Object')
+            print('[X] Failed to write the Object')
         
             
     def listTopics(self):
@@ -74,25 +91,31 @@ class Calamity:
         for x in self.payload:
             if x['meta']['label'] == eventLabel:
                 return len(x['articles'])
-        print('Label not found')
+        print('[?] Label not found')
         return 0
     
     def getEvent(self, eventLabel):
         for x in self.payload:
             if x['meta']['label'] == eventLabel:
                 return x
-        print('Event', eventLabel, 'not found')
+        print('[?] Event', eventLabel, 'not found')
     
     def peekEvent(self, eventLabel):
         for x in self.payload:
             if x['meta']['label'] == eventLabel:
                 return choice(x['articles'])['text']
-        print('Event', eventLabel, 'not found')
+        print('[?] Event', eventLabel, 'not found')
         
     def fetch(self, topic, label, year, urlfilepath=None, urllist=None):
         b = []
         if urlfilepath != None:
             urllist = Calamity.urlRead(urlfilepath)
+        
+        urllist_clean = list(set(urllist))
+        if(len(urllist_clean) < len(urllist)):
+            print('[*] Removed', len(urllist) - len(urllist_clean),'duplicate URLs')
+            urllist = urllist_clean
+        
         for x in urllist:
             a = Article(url= x.strip(), config= Calamity.config)
             try:
@@ -112,11 +135,11 @@ class Calamity:
         p = {'articles': b, 'meta': {'label' : label, 'year': year, 'topic': topic}}                
         for x in self.payload:
             if x['meta'] == p['meta']:
-                x['articles'].append(p['articles'])
+                x['articles'].extend(p['articles'])
                 break
         else:
             self.payload.append(p)
-        print('[$] Added', len(b), 'articles to', label)
+        print('[+] Added', len(b), 'articles to', label)
         
     def _tryRequests(url, articleInstance):
         r = requests.get(url, headers={'User-Agent' : 'newspaper/0.2.7'})
