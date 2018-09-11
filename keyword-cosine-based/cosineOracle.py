2e7fd5d1483661f8c1cb0c443e02e36ddb282946
Mahmoud Khaled <mahmoud_khld@yahoo.com>
2018-09-11 22:26:53 +0200

    Adding NER to CosineOracle

--- keyword-cosine-based/cosineOracle.py
+++ keyword-cosine-based/cosineOracle.py
@@ -1,4 +1,5 @@
 import numpy
+import spacy
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from nltk.stem.snowball import EnglishStemmer
@@ -6,10 +7,13 @@ from nltk.stem.snowball import EnglishStemmer
 class CosineOracle:
     stemmer = EnglishStemmer()
     analyzer = TfidfVectorizer().build_analyzer()
+    ner = spacy.load('en')
     
-    def __init__(self, train_data, test_data, tag='NO TAG'):
+    def __init__(self, train_data, test_data, tag='NO TAG', NER=False):
         self.tag = tag
-        self.vectorizer = TfidfVectorizer(stop_words='english', analyzer=CosineOracle._stemmed_words)
+        self.z = CosineOracle._ner_words if NER else CosineOracle._stemmed_words
+        self.vectorizer = TfidfVectorizer(stop_words='english',
+                                          analyzer=self.z)
         self.train_data = train_data
         self.test_data = test_data
         self._fit()
@@ -28,4 +32,7 @@ class CosineOracle:
 
     def _stemmed_words(text):
         return (CosineOracle.stemmer.stem(word) for word in CosineOracle.analyzer(text))
+    
+    def _ner_words(text):
+        return (str(word).lower().strip() for word in CosineOracle.ner(text).ents)
 
